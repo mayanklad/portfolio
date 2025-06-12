3c41afe45576e1cb30edefde2c68b707243dca4e
mayanklad <mayanklad12@gmail.com>
2025-06-11 22:19:40 -0400

    Uninstall configuration and dependency vite-aliases

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { ViteAliases } from 'vite-aliases'
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
         react(),
         tailwindcss(),
-        ViteAliases({
-            deep: false,
-        }),
     ],
 })
